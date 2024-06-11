c7cdb4f2488474a1034183dda9cbe99441a8d6e3
Levente Tóth <t.levi0828@gmail.com>
2024-06-11 20:58:26 +0200

    Leaderboard added with visual bug

--- Project-Game/Assets/PlayerStatsLoader.cs
+++ Project-Game/Assets/PlayerStatsLoader.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.UI;
+using TMPro;
 
 public class PlayerStatsLoader : MonoBehaviour
 {
     public GameObject playerStatsContent;
-    public GameObject rowPrefab; 
+    public GameObject rowPrefab;
 
     void Start()
     {
@@ -25,14 +25,12 @@ public class PlayerStatsLoader : MonoBehaviour
         else
         {
             string playersDataString = playersDataRequest.downloadHandler.text;
-            Debug.Log("Received Data: " + playersDataString);
             DisplayPlayerStats(playersDataString);
         }
     }
 
     void DisplayPlayerStats(string data)
     {
-        Debug.Log("Displaying Player Stats");
         string[] players = data.Split(';');
         foreach (string player in players)
         {
@@ -54,29 +52,17 @@ public class PlayerStatsLoader : MonoBehaviour
                     }
                 }
 
-                Debug.Log("Player Name: " + playerName + ", Player Score: " + playerScore);
-
                 if (!string.IsNullOrEmpty(playerName) && !string.IsNullOrEmpty(playerScore))
                 {
                     GameObject newRow = Instantiate(rowPrefab, playerStatsContent.transform);
-                    Debug.Log("Instantiated new row prefab: " + newRow.name);
-                    newRow.SetActive(true); // Győződj meg róla, hogy az új prefab aktív
-                    Text[] texts = newRow.GetComponentsInChildren<Text>();
-                    Debug.Log("Found " + texts.Length + " Text components in new row");
-                    foreach (Text text in texts)
-                    {
-                        Debug.Log("Found Text Component: " + text.gameObject.name);
-                        if (text.gameObject.name == "Név")
-                        {
-                            text.text = playerName;
-                            Debug.Log("Set Name Text: " + playerName + ", Component Text: " + text.text);
-                        }
-                        else if (text.gameObject.name == "Pontszám")
-                        {
-                            text.text = playerScore;
-                            Debug.Log("Set Score Text: " + playerScore + ", Component Text: " + text.text);
-                        }
-                    }
+                    newRow.SetActive(true);
+
+                    
+                    TextMeshProUGUI nameText = newRow.transform.Find("Canvas/Név").GetComponent<TextMeshProUGUI>();
+                    TextMeshProUGUI scoreText = newRow.transform.Find("Canvas/Pontszám").GetComponent<TextMeshProUGUI>();
+
+                    nameText.text = playerName;
+                    scoreText.text = playerScore;
                 }
             }
         }
